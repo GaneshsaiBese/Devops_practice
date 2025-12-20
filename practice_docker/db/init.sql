b6f5a40eccfdae90e8b4f4f9cd872ca69fffa337
GaneshSaiBese <ganeshsaibese31@gmail.com>
2025-12-20 19:27:58 +0000

    update

--- practice_docker/db/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS word_stats (
-   id SERIAL PRIMARY KEY,
-   total_words INTEGER NOT NULL
-);
-INSERT INTO word_stats (total_words)
-SELECT 0
-WHERE NOT EXISTS (SELECT 1 FROM word_stats);
